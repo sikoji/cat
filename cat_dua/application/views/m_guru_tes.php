ec435da95f8d93bddc90329a2ed864ad1c9e686a
Nur Akhwan <akhwan90@gmail.com>
2018-02-28 13:47:43 +0700

    validasai jumlah soal

--- cat_dua/application/views/m_guru_tes.php
+++ cat_dua/application/views/m_guru_tes.php
@@ -41,22 +41,34 @@
         <h4 id="myModalLabel">Buat Ujian</h4>
       </div>
       <div class="modal-body">
+        <div class="alert alert-info">
+        <a href="#" onclick="return view_petunjuk('petunjuk');">petunjuk ..?</a>
+        <div id="petunjuk">
+          <ul>
+            <li><b>Jml Soal</b>, harap dimasukkan sesuai jumlah soal yang sudah ada di bank soal</li>
+            <li><b>Tgl Mulai</b>, adalah waktu awal boleh mulai meng-klik tombol "mulai ujian"</li>
+            <li><b>Tgl Selesai</b>, adalah waktu akhir boleh mulai meng-klik tombol "mulai ujian"</li>
+            <li><b>Acak Soal</b>, jika dipilih acak, maka soal akan diacak, jika diurutkan, maka akan diurutkan berdasarkan urutan soal masuk</li>
+          </ul>
+        </div>
+      </div>
+
           <form name="f_ujian" id="f_ujian" onsubmit="return m_ujian_s();">
             <input type="hidden" name="id" id="id" value="0">
             <input type="hidden" name="jumlah_soal1" id="jumlah_soal1" value="0">
               <table class="table table-form">
                 <tr><td style="width: 25%">Nama Ujian</td><td style="width: 75%"><input type="text" class="form-control" name="nama_ujian" id="nama_ujian" required></td></tr>
-                <tr><td>Mata Pelajaran</td><td><?php echo form_dropdown('mapel', $p_mapel, '', 'onchange="return __ambil_jumlah_soal(this.value);" class="form-control"  id="mapel" required'); ?></td></tr>
+                <tr><td>Mata Pelajaran</td><td><?php echo form_dropdown('mapel', $p_mapel, '', 'class="form-control"  id="mapel" required'); ?></td></tr>
                 <tr><td>Jumlah soal</td><td><?php echo form_input('jumlah_soal', '', 'class="form-control"  id="jumlah_soal" required'); ?></td></tr>
                 <tr><td>Tgl Mulai</td><td>
-                  <input type="date" name='tgl_mulai' class="form-control" style="width: 150px; display: inline; float: left" id="tgl_mulai" placeholder="Tgl" required>
+                  <input type="date" name='tgl_mulai' class="form-control" style="width: 150px; display: inline; float: left" id="tgl_mulai" placeholder="Tgl" data-tooltip="waktu awal boleh menge-klik tombol \"mulai\" ujian" required>
                   <input type="time" name='wkt_mulai' class="form-control" style="width: 100px; display: inline; float: left" id="wkt_mulai" placeholder="Waktu" required>
                 </td></tr>
-                <tr><td>Waktu Ujian</td><td><?php echo form_input('waktu', '', 'class="form-control" id="waktu" placeholder="menit" required style="width: 100px; display: inline; float: left"'); ?> <div style="float: left; margin: 4px 0 0 10px"> menit</div></td></tr>
-                <tr><td>Keterlambatan</td><td>
+                <tr><td>Tgl Selesai</td><td>
                   <input type="date" name='terlambat' class="form-control" style="width: 150px; display: inline; float: left" id="terlambat" placeholder="Tgl" required>
                   <input type="time" name='terlambat2' class="form-control" style="width: 100px; display: inline; float: left" id="terlambat2" placeholder="Waktu" required>
                 </td></tr>
+                <tr><td>Waktu Ujian</td><td><?php echo form_input('waktu', '', 'class="form-control" id="waktu" placeholder="menit" required style="width: 100px; display: inline; float: left"'); ?> <div style="float: left; margin: 4px 0 0 10px"> menit</div></td></tr>
                 <tr><td>Acak Soal</td><td><?php echo form_dropdown('acak', $pola_tes, '', 'class="form-control"  id="acak" required'); ?></td></tr>
               </table>
       </div>
